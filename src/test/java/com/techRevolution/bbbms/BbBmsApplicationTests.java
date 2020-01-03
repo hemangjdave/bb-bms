6015aea4db65c302356a3c8448d05b62db3db2cd
Hemang Dave <hemangjdave@gmail.com>
2020-01-04 00:28:22 +0530

    This is initial Project setup for bb-bms project.

---
+++ src/test/java/com/techRevolution/bbbms/BbBmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techRevolution.bbbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbBmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
